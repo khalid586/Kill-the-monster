da53d63ee45bc2eaaed3cfb73bdcad75c441d4bd
khalid586 <khalidcomilla58@gmail.com>
2024-01-26 11:42:34 +0600

    update

--- script.js
+++ script.js
@@ -25,12 +25,28 @@ button3.onclick = fightDragon;
 
 function goStore() {
     button1.innerText = "Buy 10 health (10 gold)";
+    button2.innerText = "Buy weapon (30 gold)";
+    button3.innerText = "Go to town square";
+
+    button1.onclick = buyHealth;
+    button2.onclick = buyWeapon;
+    button3.onclick = goTown;
+    text.innerText = "You enter the store.";
+}
+function buyHealth(){
+    
+}
+  
+function buyWeapon(){
+
+}
+  
+function goTown(){
+
 }
   
 function goCave() {
-    console.log("Going to cave.");
 }
 
 function fightDragon() {
-    console.log("Fighting dragon.");
 }
