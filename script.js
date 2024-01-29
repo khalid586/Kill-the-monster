3bda8ce639021db0c30817b3aab60fb83488066e
khalid586 <khalidcomilla58@gmail.com>
2024-01-29 23:47:56 +0600

    last commit !! step 138 done

--- script.js
+++ script.js
@@ -65,6 +65,12 @@ const locations = [
         "button functions": [goTown ,goTown ,goTown ],
         text:'The monster screams "Arg!" as it dies. You gain experience points and find gold.'
     },
+    {
+        name:"lose",
+        "button text": ["REPLAY?", "REPLAY?", "REPLAY?"],
+        "button functions": [restart  ,restart  ,restart  ],
+        text:'You die. ☠️'
+    },
 ];
 const monsters = [
     {
@@ -221,4 +227,18 @@ function defeatMonster(){
 
 function lose (){
     update(locations[5]);
+}
+
+function restart(){
+    xp = 0;
+    health = 100;
+    gold = 50;
+    currentWeapon = 0;
+    inventory = ['stick'];
+
+    goldText.innerText = gold;
+    healthText.innerText = health;
+    xpText.innerText = xp;
+
+    goTown();
 }
