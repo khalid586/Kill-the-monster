239748c66a411d6325efb25c9da68ca93f6b1815
khalid586 <khalidcomilla58@gmail.com>
2024-01-29 23:43:01 +0600

    step 136 done

--- script.js
+++ script.js
@@ -58,7 +58,13 @@ const locations = [
         "button text":["Attack","Dodge","Run"],
         "button functions":[attack,dodge,goTown],
         text:"You are fighting a monster."
-    }
+    },
+    {
+        name:"kill monster",
+        "button text": ["Go to town square", "Go to town square", "Go to town square"],
+        "button functions": [goTown ,goTown ,goTown ],
+        text:'The monster screams "Arg!" as it dies. You gain experience points and find gold.'
+    },
 ];
 const monsters = [
     {
@@ -184,8 +190,35 @@ function fightDragon() {
 function attack(){
     text.innerText = "The " + monsters[fighting].name +" attacks.";
     text.innerText += " You attack it with your " +  weapons[currentWeapon].name + ".";
+    health -= monsters[fighting].level;
+    monsterHealth -= weapons[currentWeapon].power  + Math.floor(Math.random() * xp) + 1;
+    
+    healthText.innerText = health;
+    monsterHealthText.innerText = monsterHealth;
+
+    
+    if(health <= 0){
+        lose();
+    }
+    else if(monsterHealth <= 0){
+        defeatMonster();
+    }
 }
 
 function dodge(){
+    text.innerText = "You dodge the attack from the " + monsters[fighting].name + "."
+}
 
+function defeatMonster(){
+    gold += Math.floor(monsters[fighting].level * 6.7);
+    xp += monsters[fighting].level;
+
+    goldText.innerText = gold;
+    xpText.innerText = xp;
+
+    update(locations[4]);
+}
+
+function lose (){
+    update(locations[5]);
 }
